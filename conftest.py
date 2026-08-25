1529e8ef10e2b090ea6e7315c357f00d994b2173
Евгений-старый <jankovskiyevgenij@gmail.comm>
2026-08-25 19:18:51 +0300

    Add Allure environment information

--- conftest.py
+++ conftest.py
@@ -1,12 +1,35 @@
-import pytest
+import os
+import platform
 
+import pytest
+import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
+@pytest.fixture(scope="session", autouse=True)
+def allure_environment():
+    environment = {
+        "Python": platform.python_version(),
+        "OS": platform.system(),
+        "Browser": "Google Chrome",
+        "Selenium": selenium.__version__,
+        "Pytest": pytest.__version__,
+    }
+
+    os.makedirs("allure-results", exist_ok=True)
+
+    with open(
+        "allure-results/environment.properties",
+        "w",
+        encoding="utf-8",
+    ) as file:
+        for key, value in environment.items():
+            file.write(f"{key}={value}\n")
+
+
 @pytest.fixture
 def driver():
-
     options = Options()
 
     options.add_argument("--start-maximized")
